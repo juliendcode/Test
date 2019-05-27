7dbc032129dd5a7a291cc667a2f57cbbfef94aee
juliendcode <juliendcode@gmail.com>
2019-05-27 14:51:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit {
+  title = 'Liste des pokémons';
+
+  private pokemons: Pokemon[];
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    alert('vous avez cliqué sur ' + pokemon.name);
+  }
 }
